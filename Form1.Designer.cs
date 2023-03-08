3a296d40b90fd6f205a95e1d07f6944e696205aa
Jeremi <jeremi.lipiec@gmail.com>
2023-03-08 19:38:15 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿
+namespace Net_Tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer_ping = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MainPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.label_status = new System.Windows.Forms.Label();
+            this.last_text = new System.Windows.Forms.Label();
+            this.label_avgPing = new System.Windows.Forms.Label();
+            this.button_start = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.result_text = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.MainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Net Tester";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
+            // 
+            // timer_ping
+            // 
+            this.timer_ping.Enabled = true;
+            this.timer_ping.Interval = 1000;
+            this.timer_ping.Tick += new System.EventHandler(this.timer_ping_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel2.Controls.Add(this.flowLayoutPanel2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(287, 36);
+            this.panel2.TabIndex = 9;
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoSize = true;
+            this.flowLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel2.Controls.Add(this.button2);
+            this.flowLayoutPanel2.Controls.Add(this.button1);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(225, 2);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(62, 31);
+            this.flowLayoutPanel2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 25);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(34, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 25);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Net Tester v2.6.5";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.MainPanel.Controls.Add(this.label_status);
+            this.MainPanel.Controls.Add(this.last_text);
+            this.MainPanel.Controls.Add(this.label_avgPing);
+            this.MainPanel.Controls.Add(this.button_start);
+            this.MainPanel.Controls.Add(this.label2);
+            this.MainPanel.Controls.Add(this.result_text);
+            this.MainPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.MainPanel.Location = new System.Drawing.Point(0, 36);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(287, 406);
+            this.MainPanel.TabIndex = 10;
+            // 
+            // label_status
+            // 
+            this.label_status.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_status.ForeColor = System.Drawing.Color.Black;
+            this.label_status.Location = new System.Drawing.Point(3, 0);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(284, 31);
+            this.label_status.TabIndex = 1;
+            this.label_status.Text = "Status: Paused";
+            this.label_status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // last_text
+            // 
+            this.last_text.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.last_text.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.last_text.ForeColor = System.Drawing.Color.Black;
+            this.last_text.Location = new System.Drawing.Point(3, 31);
+            this.last_text.Name = "last_text";
+            this.last_text.Size = new System.Drawing.Size(284, 53);
+            this.last_text.TabIndex = 9;
+            this.last_text.Text = "0 ms";
+            this.last_text.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_avgPing
+            // 
+            this.label_avgPing.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_avgPing.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_avgPing.ForeColor = System.Drawing.Color.Black;
+            this.label_avgPing.Location = new System.Drawing.Point(3, 84);
+            this.label_avgPing.Name = "label_avgPing";
+            this.label_avgPing.Size = new System.Drawing.Size(284, 34);
+            this.label_avgPing.TabIndex = 6;
+            this.label_avgPing.Text = "Average: 0ms";
+            this.label_avgPing.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button_start
+            // 
+            this.button_start.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button_start.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button_start.FlatAppearance.BorderSize = 0;
+            this.button_start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_start.Font = new System.Drawing.Font("Century", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button_start.Image = ((System.Drawing.Image)(resources.GetObject("button_start.Image")));
+            this.button_start.Location = new System.Drawing.Point(78, 121);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(134, 129);
+            this.button_start.TabIndex = 0;
+            this.button_start.Text = "Start\r\n";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(3, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(284, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Last 5:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // result_text
+            // 
+            this.result_text.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.result_text.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result_text.ForeColor = System.Drawing.Color.Black;
+            this.result_text.Location = new System.Drawing.Point(3, 277);
+            this.result_text.Name = "result_text";
+            this.result_text.Size = new System.Drawing.Size(284, 115);
+            this.result_text.TabIndex = 10;
+            this.result_text.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(287, 442);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.panel2);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Net Tester v1.5.1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.MainPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Timer timer_ping;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FlowLayoutPanel MainPanel;
+        private System.Windows.Forms.Label label_status;
+        private System.Windows.Forms.Label last_text;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_avgPing;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label result_text;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Net_Tester
+{
+    struct PingResult
+    {
+        public string target;
+        public string result;
+        public int time;
+    }
+
+    enum PingState
+    {
+        Paused,
+        Pinging,
+        NetworkError
+    }
+
+    public partial class Form1 : Form
+    {
+        PingState _state = PingState.Paused;
+        PingState State {
+            get => _state;
+            set {
+                _state = value;
+
+                if(value == PingState.Pinging)
+                    button_start.Text = "Stop";
+                else if (value == PingState.Paused)
+                    button_start.Text = "Start";
+
+                label_status.Text = $"Status: {State}";
+
+                UpdateBackColor();
+            }
+        }
+
+        readonly string pingAdress = "8.8.8.8";
+        readonly string dayFilePath = "dailyHistory";
+        readonly string folderPath = "tests";
+        readonly string extension = ".txt";
+        readonly string today = "";
+
+        PingResult[] history;
+        readonly int historyLimit = 5;
+
+        Ping test = new Ping();
+
+        public Form1() {
+            InitializeComponent();
+            ShowInTaskbar = false;
+            notifyIcon.Visible = true;
+
+            history = new PingResult[historyLimit];
+
+            today = DateTime.Now.Date.ToShortDateString();
+            MainPanel.BackColor = Color.Gray;
+        }
+
+        private void button_start_Click(object sender, EventArgs e) {
+            if (State == PingState.Paused)
+                State = PingState.Pinging;
+            else State = PingState.Paused;
+        }
+
+        void CheckPing() {
+            if (State == PingState.Paused) return;
+            PingReply reply;
+
+            try {
+                reply = test.Send(pingAdress);
+            } catch {
+                State = PingState.NetworkError;
+                return;
+            }
+
+            PingResult result = new PingResult() {
+                target = pingAdress
+            };
+
+            if (reply.Status == IPStatus.Success) {
+                result.result = $"{reply.RoundtripTime} ms";
+                result.time = (int)reply.RoundtripTime;
+                State = PingState.Pinging;
+            } else {
+                result.result = "Net error";
+                State = PingState.NetworkError;
+                result.time = -1;
+            }
+
+            AddToHistory(result);
+            Save();
+            UpdateGUI();
+        }
+
+        void AddToHistory(PingResult result) {
+            PingResult[] demo = new PingResult[history.Length];
+            demo[0] = result;
+
+            for (int i = 1; i < history.Length; i++) {
+                demo[i] = history[i - 1];
+            }
+
+            for (int i = 0; i < history.Length; i++) {
+                history[i] = demo[i];
+            }
+
+            UpdateBackColor();
+        }
+
+        void UpdateBackColor() {
+            switch (State) {
+                case PingState.Paused: {
+                    MainPanel.BackColor = Color.Gray;
+                    return;
+                }
+                case PingState.NetworkError: {
+                    MainPanel.BackColor = Color.Red;
+                    return;
+                }
+            }
+
+            int lastPing = history[0].time;
+
+            if (lastPing < 100) {
+                MainPanel.BackColor = Color.Green;
+            } else if (lastPing < 250) {
+                MainPanel.BackColor = Color.Yellow;
+            } else {
+                MainPanel.BackColor = Color.Red;
+            }
+        }
+
+        float CalculateAvgPing() {
+            float result = 0;
+            int fullness = 0;
+
+            for (int i = 0; i < history.Length; i++) {
+                if (history[i].time > 0) {
+                    fullness++;
+                    result += history[i].time;
+                }
+            }
+            return result / fullness;
+        }
+
+        void Save() {
+            try {
+                if (!Directory.Exists(folderPath)) {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                // to history
+                StreamWriter sw = File.AppendText(folderPath + "/" + dayFilePath + today + extension);
+                sw.WriteLine(history[0].time);
+                sw.Close();
+            } catch { } // just skip
+        }
+
+        private void timer_ping_Tick(object sender, EventArgs e) => CheckPing();
+
+        private void notifyIcon_DoubleClick(object sender, EventArgs e) {
+            WindowState = FormWindowState.Normal;
+            Show();
+            Activate();
+        }
+
+        void UpdateGUI() {
+            label_avgPing.Text = $"Average: " + CalculateAvgPing().ToString("n0") + "ms";
+
+            //update last
+            last_text.Text = history[0].result;
+
+            // update history
+            string result = "";
+            for (int i = 0; i < history.Length; i++)
+                result += history[i].result + "\n";
+            result_text.Text = result;
+        }
+
+
+        //mouse move
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void panel2_MouseMove(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) => Application.Exit();
+
+        private void button2_Click(object sender, EventArgs e) {
+            WindowState = FormWindowState.Minimized;
+        }
+    }
+}
